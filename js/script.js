e55a1725b5bebb407a9996e2c5a3f187a2751410
Ade Ronialta <ade.ronialta@gmail.com>
2023-05-28 12:37:44 +0700

    membuat website desa wisata claket

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+
+// progress bar
+const filled = document.querySelector(".filled");
+
+function update() {
+  filled.style.width = `${
+    (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100
+  }%`;
+  requestAnimationFrame(update);
+}
+update();
+
+// ketika humburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik di luar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
